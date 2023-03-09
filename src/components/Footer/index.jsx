612d10a42af673e63072ffee6eb2b14ff3402c87
deyvisnvg <deyvisnvg@gmail.com>
2023-03-08 23:28:58 -0500

    feat(Root): Implement component footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <div className="credits">
+            Designed with ❤ by
+            <a href="https://bootstrapmade.com/"> Deyvisnvg</a>
+        </div>
+    )
+}
