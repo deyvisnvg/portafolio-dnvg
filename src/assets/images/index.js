1019eee331766b811b87138a735c1e5a3f96822e
deyvis <deyvisnvg@gmail.com>
2024-01-08 19:51:29 -0500

    feat(deyvisnvg): Image export implementation

---
+++ src/assets/images/index.js
@@ -0,0 +1,28 @@
+export { default as bolsaTrabajo } from "./bolsaTrabajo.png";
+export { default as bookPokemon } from "./bookPokemon.png";
+export { default as buildsoft } from "./buildsoft.png";
+
+export { default as conf2 } from "./conf2.jpeg";
+export { default as cssGrid } from "./cssGrid.png";
+export { default as control } from "./control.png";
+export { default as cajeroAutomatico } from "./cajeroAutomatico.png";
+export { default as chatSocketio } from "./chatSocketio.png";
+export { default as clonTiktok } from "./clonTiktok.png";
+
+export { default as dibujo } from "./dibujo.png";
+export { default as dibujarConFlechas } from "./dibujarConFlechas.png";
+
+export { default as juegoSecuencia } from "./juegoSecuencia.png";
+
+export { default as leafletMapa } from "./leafletMapa.png";
+
+export { default as paintWeb } from "./paintWeb.png";
+
+export { default as rickyMorty } from "./rickyMorty.png";
+
+export { default as silabuz } from "./silabuz.jpg";
+
+export { default as todos } from "./todos.png";
+export { default as twitterClonFollow } from "./twitterClonFollow.png";
+
+export { default as villaNeyser } from "./villaNeyser.png";
